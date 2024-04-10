484f525b80013d09fc2fd0d2557397dde87b45a7
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-04-10 17:13:33 -0400

    format

--- tests/distributions/test_folded_normal.py
+++ tests/distributions/test_folded_normal.py
@@ -3,8 +3,8 @@ from rs_distributions.distributions.folded_normal import FoldedNormal
 import torch
 
 
-@pytest.mark.parametrize('batch_shape', [1, 10])
-@pytest.mark.parametrize('sample_shape', [(), (10,)])
+@pytest.mark.parametrize("batch_shape", [1, 10])
+@pytest.mark.parametrize("sample_shape", [(), (10,)])
 def test_folded_normal_execution(batch_shape, sample_shape):
     params = torch.ones((2, batch_shape), requires_grad=True)
     loc, scale = params
