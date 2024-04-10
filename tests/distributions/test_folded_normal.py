709f282872ca0ee1f23ed373afbecb1fc78cc46a
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-04-10 17:12:09 -0400

    add FoldedNormal execution test

---
+++ tests/distributions/test_folded_normal.py
@@ -0,0 +1,18 @@
+import pytest
+from rs_distributions.distributions.folded_normal import FoldedNormal
+import torch
+
+
+@pytest.mark.parametrize('batch_shape', [1, 10])
+@pytest.mark.parametrize('sample_shape', [(), (10,)])
+def test_folded_normal_execution(batch_shape, sample_shape):
+    params = torch.ones((2, batch_shape), requires_grad=True)
+    loc, scale = params
+    q = FoldedNormal(loc, scale)
+    z = q.rsample(sample_shape)
+    q.mean()
+    q.variance()
+    q.cdf(z)
+    q.pdf(z)
+    q.log_prob(z)
+    torch.autograd.grad(z.sum(), params)
