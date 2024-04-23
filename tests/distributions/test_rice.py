f578576e3303b6e09d8f5f0bc66cfb8728962d43
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-04-23 12:07:29 -0400

    test broadcasting with float, rename test

--- tests/distributions/test_rice.py
+++ tests/distributions/test_rice.py
@@ -3,11 +3,14 @@ from rs_distributions.distributions.rice import Rice
 import torch
 
 
+@pytest.mark.parametrize("test_float_broadcasting", [False, True])
 @pytest.mark.parametrize("batch_shape", [1, 10])
 @pytest.mark.parametrize("sample_shape", [(), (10,)])
-def test_folded_normal_execution(batch_shape, sample_shape):
+def test_rice_execution(test_float_broadcasting, batch_shape, sample_shape):
     params = torch.ones((2, batch_shape), requires_grad=True)
     loc, scale = params
+    if test_float_broadcasting:
+        loc = 1.
     q = Rice(loc, scale)
     z = q.rsample(sample_shape)
     q.mean
