fix folded normal broadcasting

src/rs_distributions/distributions/folded_normal.py
@@ -35,8 +35,7 @@ class FoldedNormal(dist.Distribution):
     support = torch.distributions.constraints.nonnegative
 
     def __init__(self, loc, scale, validate_args=None):
-        self.loc = torch.as_tensor(loc)
-        self.scale = torch.as_tensor(scale)
+        self.loc,self.scale = torch.distributions.utils.broadcast_all(loc, scale)
         batch_shape = self.loc.shape
         super().__init__(batch_shape, validate_args=validate_args)
         self._irsample = NormalIRSample().apply

src/rs_distributions/distributions/rice.py
@@ -53,8 +53,7 @@ class Rice(dist.Distribution):
     support = torch.distributions.constraints.nonnegative
 
     def __init__(self, nu, sigma, validate_args=None):
-        self.nu = torch.as_tensor(nu)
-        self.sigma = torch.as_tensor(sigma)
+        self.nu,self.sigma = torch.distributions.utils.broadcast_all(nu, sigma)
         batch_shape = self.nu.shape
         super().__init__(batch_shape, validate_args=validate_args)
         self._irsample = RiceIRSample().apply
@@ -114,10 +113,13 @@ class Rice(dist.Distribution):
         """
         shape = self._extended_shape(sample_shape)
         nu, sigma = self.nu, self.sigma
-        A = sigma * torch.randn(shape, dtype=self.nu.dtype, device=self.nu.device) + nu
-        B = sigma * torch.randn(shape, dtype=self.nu.dtype, device=self.nu.device)
-        z = torch.sqrt(A * A + B * B)
-        return z
+        nu = nu.expand(shape)
+        sigma = sigma.expand(shape)
+        with torch.no_grad():
+            A = torch.normal(nu, sigma)
+            B = torch.normal(torch.zeros_like(nu), sigma)
+            z = torch.sqrt(A * A + B * B)
+            return z
 
     @property
     def mean(self):

tests/distributions/test_folded_normal.py
@@ -3,11 +3,14 @@ from rs_distributions.distributions.folded_normal import FoldedNormal
 import torch
 
 
-@pytest.mark.parametrize("batch_shape", [1, 10])
+@pytest.mark.parametrize("test_float_broadcasting", [False, True])
+@pytest.mark.parametrize("batch_shape", [3, 10])
 @pytest.mark.parametrize("sample_shape", [(), (10,)])
-def test_folded_normal_execution(batch_shape, sample_shape):
+def test_folded_normal_execution(test_float_broadcasting, batch_shape, sample_shape):
     params = torch.ones((2, batch_shape), requires_grad=True)
     loc, scale = params
+    if test_float_broadcasting:
+        loc = 1.
     q = FoldedNormal(loc, scale)
     z = q.rsample(sample_shape)
     q.mean

tests/distributions/test_rice.py
@@ -4,7 +4,7 @@ import torch
 
 
 @pytest.mark.parametrize("test_float_broadcasting", [False, True])
-@pytest.mark.parametrize("batch_shape", [1, 10])
+@pytest.mark.parametrize("batch_shape", [3, 10])
 @pytest.mark.parametrize("sample_shape", [(), (10,)])
 def test_rice_execution(test_float_broadcasting, batch_shape, sample_shape):
     params = torch.ones((2, batch_shape), requires_grad=True)